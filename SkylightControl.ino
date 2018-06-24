ee1d56be357a8fd099a382624fec8da507e3c869
dccharacter <dccharacter@gmail.com>
2018-06-24 18:19:43 -0400

    Added license and some basic commentary to the code

--- SkylightControl.ino
+++ SkylightControl.ino
@@ -1,3 +1,20 @@
+/*  Arduino firmware for mechanic sky light opener
+ *  Copyright (C) 2018  dccharacter
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version. 
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* https://github.com/cyborg5/IRLib2 */  
 //#include <IRLibRecvPCI.h>
 #include "IRLibAll.h" 
 
@@ -26,8 +43,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-  pinMode(redLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
+  //pinMode(redLed, OUTPUT);
+  //pinMode(greenLed, OUTPUT);
   
   pinMode(stepperEn, OUTPUT);
   pinMode(stepperDir, OUTPUT);
@@ -40,19 +57,17 @@ void setup() {
   myReceiver.blink13(true);
 }
 
-void pulseStepper (int nPulses) {
-  
+void pulseStepper (int nPulses) {  
   for (int i=0; i<nPulses; i++) {
     digitalWrite(stepperStep, HIGH);
     delayMicroseconds(20);
     digitalWrite(stepperStep, LOW);
     delay(step_delay);
-  }
-  
+  }  
 }
 
 void setDirection(bool dir) {
-    digitalWrite(stepperDir, dir);    
+  digitalWrite(stepperDir, dir);    
 }
 
 void enableStepper(void) {
@@ -81,29 +96,32 @@ void loop() {
     myDecoder.dumpResults(false);  //the results of a Now print results. 
 
     switch(myDecoder.value) { 
-        case 0x77E1D0C9:
-        case 0x77E1E0C9:
+      /* Rise the skylight */
+        case 0x77E1D0C9:  // Apple remote "up"
+        case 0x77E1E0C9:  // Apple remote "right"
           /*digitalWrite(redLed, HIGH);
           digitalWrite(greenLed, LOW);*/
-          step_delay = 7;
+          step_delay = 7; // This is needed for my skylight as rising it a tough job for my stepper and it starts skipping on higher speed
           up_or_down = HIGH;
           setDirection(up_or_down);
           enableStepper();
           moving = 1;
           break; 
-        case 0x77E1B0C9:
-        case 0x77E110C9:
-          digitalWrite(greenLed, HIGH);
-          digitalWrite(redLed, LOW);
-          step_delay = 3;
+      /* Lower the skylight */
+        case 0x77E1B0C9: // Apple remote "down"
+        case 0x77E110C9: // Apple remote "left"
+          //digitalWrite(greenLed, HIGH);
+          //digitalWrite(redLed, LOW);
+          step_delay = 3; // This is needed for my skylight as rising it a tough job for my stepper and it starts skipping on higher speed
           up_or_down = LOW;
           setDirection(up_or_down);
           enableStepper();
           moving = 1;
           break; 
-        case 0x77E140C9:
-        case 0x77E17AC9:
-        case 0x77E120C9:
+      /* Stop whatever we are doing */
+        case 0x77E140C9: // Apple remote "menue"
+        case 0x77E17AC9: // Apple remote "play/pause"
+        case 0x77E120C9: // Apple remote "play/pause"
           disableStepper(); 
           moving = 0;
           break; 
